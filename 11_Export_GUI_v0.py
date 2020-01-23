e6b5d391714914122ede757507d727210545734b
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-01-23 14:13:03 +1300

    Assembled Converter created but not yet edited.

--- 11_Export_GUI_v0.py
+++ 11_Export_GUI_v0.py
@@ -11,12 +11,14 @@ class Converter:
         background_color = "light blue"
 
         # Converter Main Screen GUI...
-        self.converter_frame = Frame(width=300, height=300, bg=background_color,
+        self.converter_frame = Frame(width=300, height=300,
+                                     bg=background_color,
                                      pady=10)
         self.converter_frame.grid()
 
         # Temperature Conversion Heading (row 0)
-        self.temp_converter_label = Label(self.converter_frame, text="Temperature Converter",
+        self.temp_converter_label = Label(self.converter_frame,
+                                          text="Temperature Converter",
                                           font=("Arial", "16", "bold"),
                                           bg=background_color,
                                           padx=10, pady=10)
@@ -32,7 +34,6 @@ class Converter:
         get_export = Export(self)
 
 
-
 class Export:
     def __init__(self, partner):
 
@@ -44,8 +45,10 @@ class Export:
         # Sets up child window (ie: export box)
         self.export_box = Toplevel()
 
-        # If users press cross at top, closes export and 'releases' export button
-        self.export_box.protocol('WM_DELETE_WINDOW', partial(self.close_export, partner))
+        # If users press cross at top, closes export and
+        # 'releases' export button
+        self.export_box.protocol('WM_DELETE_WINDOW',
+                                 partial(self.close_export, partner))
 
 
         # Set up GUI Frame
@@ -53,19 +56,33 @@ class Export:
         self.export_frame.grid()
 
         # Set up Export heading (row 0)
-        self.how_heading = Label(self.export_frame, text="Export / Instructions",
+        self.how_heading = Label(self.export_frame,
+                                 text="Export / Instructions",
                                  font="arial 14 bold", bg=background)
         self.how_heading.grid(row=0)
 
         # Export Instructions (label, row 1)
-        self.export_text = Label(self.export_frame, text="Enter a filename in the box below and press the Save button to save your calculation history to a text file.",
-                               justify=LEFT, width=40, bg=background, wrap=250)
+        self.export_text = Label(self.export_frame, text="Enter a filename"
+                                                         "in the box below "
+                                                         "and press the Save "
+                                                         "button to save your "
+                                                         "calculation history "
+                                                         "to a text file.",
+                               justify=LEFT, width=40,
+                                 bg=background, wrap=250)
         self.export_text.grid(row=1)
 
         # Warning text (label, row 2)
-        self.export_text = Label(self.export_frame, text="If the filename you enter below already exists, its contents will be replaced with your calculation history",
+        self.export_text = Label(self.export_frame, text="If the filename "
+                                                         "you enter below "
+                                                         "already exists, "
+                                                         "its contents will "
+                                                         "be replaced with "
+                                                         "your calculation "
+                                                         "history",
                                justify=LEFT, bg="#ffafaf", fg="maroon",
-                                 font="Arial 10 italic", wrap=225, padx=10, pady=10)
+                                 font="Arial 10 italic", wrap=225, padx=10,
+                                 pady=10)
         self.export_text.grid(row=2, pady=10)
 
         # Filename Entry Box (row 3)
@@ -82,7 +99,7 @@ class Export:
         self.save_button.grid(row=0, column=0)
 
         self.cancel_button = Button(self.save_cancel_frame, text="Cancel",
-                                    command=partial(self.close_export, partner))
+                                command=partial(self.close_export, partner))
         self.cancel_button.grid(row=0, column=1)
 
 
